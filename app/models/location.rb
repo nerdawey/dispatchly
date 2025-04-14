2293668429482c1f75884aae1f31e638690b9082
nerdawey <nerdawy@icloud.com>
2025-04-14 17:27:56 +0200

    v2

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,6 @@ class Location < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
 
-  has_many :orders
+  has_many :pickup_orders, class_name: "Order", foreign_key: "pickup_location_id"
+  has_many :dropoff_orders, class_name: "Order", foreign_key: "dropoff_location_id"
 end
